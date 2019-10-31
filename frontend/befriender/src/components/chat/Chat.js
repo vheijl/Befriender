e5ee73e099b5940999ec39776a59d617d13a036c
Victor Heijler <victor.heijler@se.experis.com>
2019-10-31 23:42:34 +0100

    Add 'hack' to force the chathistory to update on submitted text..

--- frontend/befriender/src/components/chat/Chat.js
+++ frontend/befriender/src/components/chat/Chat.js
@@ -10,11 +10,11 @@ function Chat(props) {
   // List of friends
   const [stateFriends, setFriends] = useState([]);
   const [currentFriend, setCurrentFriend] = useState(null);
+  const [update, setUpdate] = useState(0);
 
   let setFriend = (friend) => setCurrentFriend(friend);
 
   useEffect(() => {
-
     if (sessionStorage.getItem("user")) {
       fetch(`http://localhost:3001/api/friend/all/${sessionStorage.getItem("user")}`)
         .then(results => results.json())
@@ -25,16 +25,20 @@ function Chat(props) {
     }
   }, [])
 
+  const handleUpdate = () => {
+    setUpdate(update+1);
+  }
+
   let show = sessionStorage.getItem("user") ? true : false;
 
   return (
     <div className={show ? "chat" : ""}>
       {show &&
         <>
-          <ChatHistory friend={currentFriend} />
-          <Friends onSelect={setFriend} data={stateFriends} />
+          <ChatHistory friend={currentFriend} update={update} />
+          <Friends friend={currentFriend} onSelect={setFriend} data={stateFriends} />
           {currentFriend && <Info friend={currentFriend} />}
-          <SendText friend={currentFriend} />
+          <SendText friend={currentFriend} onSubmit={handleUpdate} />
         </>}
         {!show && <h1>Please login to view this page.</h1>}
 
