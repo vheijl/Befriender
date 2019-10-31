1c901761ad01035b55fcf4236bb217e64f146545
Victor Heijler <victor.heijler@se.experis.com>
2019-10-31 23:45:35 +0100

    Add and handle a fetch for the search component

--- frontend/befriender/src/components/search/Search.js
+++ frontend/befriender/src/components/search/Search.js
@@ -1,47 +1,56 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Person from './Person';
 
 function Search() {
 
-    // Used to keep track of search keyword
-    const [stateSearch, setSearch] = useState({search:""});
+  // Used to keep track of search keyword
+  const [stateSearch, setSearch] = useState({ search: "" });
 
-    function handleSearch (event) {
-      setSearch({[event.target.name] : event.target.value})
-    }
-
-    // Used to keep track of people in search
-    const [peopleState, setPeople] = useState([]);
-
-    function searchUser(event) {
-      event.preventDefault();
-      console.log(setSearch);
-
-      fetch("http://localhost:3001/api/user/search", {method:"POST", body:JSON.stringify(stateSearch)})
-      .then(results => results.json())
-      .then((data)=> setPeople(data)
-      )
-    }
-
-    return (
-      <div className="login">
-         
-         <form onChange={searchUser}>
-            <h1> Search for people to befriend </h1>
-            <div className="">
-                <input onChange={handleSearch} name="search" type="text" placeholder="Search name or description" />
-            </div>
-
-          </form>
-            
-            <ul>
-                {peopleState.map( i => {
-                    return <li> <Person personData={{i}}/> </li>
-                })}
-            </ul>      
-
-      </div>     
-    );
+  function handleSearch(event) {
+    setSearch({ [event.target.name]: event.target.value })
   }
-  
-  export default Search;
+
+  // Used to keep track of people in search
+  const [peopleState, setPeople] = useState([]);
+
+  function searchUser(event) {
+    event.preventDefault();
+    console.log(setSearch);
+
+    if (stateSearch.search.length > 0) {
+      fetch(`http://localhost:3001/api/search/keyword`, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({userId: sessionStorage.getItem("user"), keyword: stateSearch.search})
+    })
+        .then(results => results.json())
+        .then((data) => {
+          console.log(data);
+          setPeople(data);
+        })
+    }
+  }
+
+  return (
+    <div className="login">
+
+      <form onChange={searchUser}>
+        <h1> Search for people to befriend </h1>
+        <div className="">
+          <input onChange={handleSearch} name="search" type="text" placeholder="Search name or description" />
+        </div>
+
+      </form>
+
+        {peopleState.map(person => {
+          return <Person personData={person} />
+        })}
+
+    </div>
+  );
+}
+
+export default Search;
