b8298218ecda89a9bc2118363cdf3fad44d8c2cf
Victor Heijler <victor.heijler@se.experis.com>
2019-10-31 23:44:55 +0100

    Write out person in search results and add a button for user to add person as a friend

--- frontend/befriender/src/components/search/Person.js
+++ frontend/befriender/src/components/search/Person.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function Person(event) {
+function Person(props) {
+
+    const [message, setMessage] = useState("Add as a friend");
+
+    const handleClick = id => {
+        fetch(`http://localhost:3001/api/friend/add`, {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({friend1: sessionStorage.getItem("user"), friend2: id})
+        })
+            .then(result => result.json())
+            .then(data => {
+                setMessage(data.message);
+            })
+    }
 
     return (
-      <div className="person">
-          <h1> props.personData.email </h1>
-          <h2> props.personData.email  </h2>
-          <h2> props.personData.description  </h2>
-      </div>     
-  );
-  }
-  
-  export default Person;
+        <div className="person">
+            <h1>{props.personData.username}</h1>
+            <p>{props.personData.email}</p>
+            <p>{props.personData.description}</p>
+            <button onClick={event => handleClick(props.personData.id)}>{message}</button>
+        </div>
+    );
+}
 
-  
+export default Person;
+
