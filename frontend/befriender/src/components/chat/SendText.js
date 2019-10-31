ebefa5e354f7002462ea967bfcafa5dec9157f12
Victor Heijler <victor.heijler@se.experis.com>
2019-10-31 23:43:44 +0100

    Send message on submit

--- frontend/befriender/src/components/chat/SendText.js
+++ frontend/befriender/src/components/chat/SendText.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function SendText() {
-    return (
-      <div className="sendText">
-         <h1> SendText </h1>
-      </div>
-     
-  );
+function SendText(props) {
+
+  const [input, setInput] = useState({message: ""});
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    fetch(`http://localhost:3001/api/message/post`, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ from_id: sessionStorage.getItem("user"), to_id: props.friend.id, type: 0, message: input.message })
+    })
+      .then(result => result.json())
+      .then(data => {
+        // On send!
+        setInput({ message: "" });
+        props.onSubmit();
+      })
   }
-  
-  export default SendText;
+
+  const handleChange = event => {
+    setInput({ ...input, [event.target.name]: event.target.value });
+  }
+
+
+  return (
+    <div className="sendText">
+      <form onSubmit={handleSubmit}>
+        <input value={input.message} onChange={handleChange} name="message" type="text" />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+
+  );
+}
+
+export default SendText;
